24eae5f9a9fdbe6d15e73c6fe3fcc01c33f497e4
Đỗ Hồng Quân <quandohong28@gmail.com>
2025-07-19 03:15:29 +0700

    fix some bugs

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <div>
+        <button class="mb-2 me-2 rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Hello</button>
+    </div>
+@endsection
